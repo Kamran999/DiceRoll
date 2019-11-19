bd567ca747d105973df7eba998d429b98886fb1b
KamranAnjum <tkamrannanjum@gmail.com>
2019-11-19 17:47:53 +0400

    DiceRoller

---
+++ app/src/main/java/com/example/dicerollar/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.dicerollar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import java.util.*
+
+/**
+ * DiceRoller demonstrates simple interactivity in an Android app.
+ * It contains one button that updates a text view with a random
+ * value between 1 and 6.
+ */
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Get the Image view from the layout and assign it to diceImage
+        diceImage = findViewById(R.id.dice_image)
+
+        // Get the Button view from the layout and assign a click
+        // listener to it.
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    /**
+     * Click listener for the Roll button.
+     */
+    private fun rollDice() {
+
+        val randomInt = Random().nextInt(6) + 1
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+}
